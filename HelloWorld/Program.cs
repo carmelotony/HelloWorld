dff617c549d57d2a2886abf6372605d21c5058c8
carmelotony <33266466+carmelotony@users.noreply.github.com>
2019-01-15 13:38:47 -0500

    Version2 HelloWorld

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //pause to display Hello World!
+            Console.WriteLine("Press any key to continue.");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
